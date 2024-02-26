ef9eb758ca6110c9e4a228071d06713b6fafab96
MLuisa91 <ml.fernandezgon@gmail.com>
2024-02-27 00:06:40 +0100

    Subida 27/02/2024

--- src/main/java/com/example/aviones/vista/Main.java
+++ src/main/java/com/example/aviones/vista/Main.java
@@ -1,19 +1,39 @@
 package com.example.aviones.vista;
 
+import com.example.aviones.controlador.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    @Override
+    /*@Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/aviones/vista/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/aviones/vista/sample.fxml"));
         Scene escena = new Scene(root);
         stage.setScene(escena);
         stage.show();
+    }*/
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/aviones/vista/sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        Scene scene = new Scene(root);
+
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
+        primaryStage.setScene(scene);
+
+        controller.setStage(primaryStage);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
